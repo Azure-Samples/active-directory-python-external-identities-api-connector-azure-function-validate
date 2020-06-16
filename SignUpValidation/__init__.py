483ec3ba79bc87da49a74692c6fa89682dd1a679
Edwin Gomez Cuellar <edgomezc@microsoft.com>
2020-06-16 02:01:09 -0400

    Cleaning up response types

--- SignUpValidation/__init__.py
+++ SignUpValidation/__init__.py
@@ -3,33 +3,39 @@ import json
 import azure.functions as func
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request v1.0.')
+def main(req: func.HttpRequest) -> func.HttpResponse:  # API version
+    API_VERSION = "0.0.1"
+
+    logging.info(
+        f"Python HTTP trigger function processed a request, API version: {API_VERSION}")
 
     # Allowed domains
-    allowedDomain = ["fabrikam.com" ,"contoso.com"];
+    allowed_domains = ["fabrikam.com", "fabricam.com"]
 
     # Get the request body
     try:
         req_body = req.get_json()
     except:
         return func.HttpResponse(
-            json.dumps({"version": "1.0.0", "status": 200, "action": "ShowBlockPage", "code": "SingUp-Validation-01", "userMessage": "Invalid input data."}),
+            json.dumps({"version": API_VERSION, "action": "ShowBlockPage",
+                        "code": "SignUp-Validation-01", "userMessage": "There was a problem with your request."}),
             status_code=200,
             mimetype="application/json"
-         )
+        )
 
     # Print out the request body
     logging.info(f"Request body: {req_body}")
 
     # Get the current user language
-    language = req_body.get('ui_locales') if 'ui_locales' in req_body and req_body.get('ui_locales') else "default" 
+    language = req_body.get('ui_locales') if 'ui_locales' in req_body and req_body.get(
+        'ui_locales') else "default"
     logging.info(f"Current language: {language}")
 
-    # If email claim not found, show validation error message. So, user can fix the input data
+    # If email claim not found, show block page. Email is required and sent by default.
     if 'email' not in req_body or not req_body.get('email') or "@" not in req_body.get('email'):
         return func.HttpResponse(
-            json.dumps({"version": "1.0.0", "status": 200, "action": "ShowBlockPage", "code": "SingUp-Validation-02", "userMessage": "Email is mandatory."}),
+            json.dumps({"version": API_VERSION, "action": "ShowBlockPage",
+                        "code": "SignUp-Validation-02", "userMessage": "Email is mandatory."}),
             status_code=200,
             mimetype="application/json"
         )
@@ -39,30 +45,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(f"Current doamin: {domain}")
 
     # Check the domain in the allowed list
-    if domain not in allowedDomain:
+    if domain not in allowed_domains:
         s = ", "
         return func.HttpResponse(
-            json.dumps({"version": "1.0.0", "status": 200, "action": "ShowBlockPage", "code": "SingUp-Validation-02", "userMessage": f"You must have an account from '{s.join(allowedDomain)}' to register as an external user for Contoso."}),
+            json.dumps({"version": API_VERSION, "action": "ShowBlockPage", "code": "SignUp-Validation-03",
+                        "userMessage": f"You must have an account from '{s.join(allowed_domains)}' to register as an external user for Contoso."}),
             status_code=200,
             mimetype="application/json"
         )
 
-    # If jobTitle claim not found, show validation error message. So, user can fix the input data
-    if 'jobTitle' not in req_body or not req_body.get('jobTitle'):
-        return func.HttpResponse(
-            json.dumps({"version": "1.0.0", "status": 200, "action": "ShowBlockPage", "code": "SingUp-Validation-02", "userMessage": "Display name is mandatory."}),
-            status_code=200,
-            mimetype="application/json"
-         )
+    # If jobTitle claim is too short, show validation error message so that user can fix error.
+    if ('jobTitle' in req_body and req_body.get('jobTitle')):  # use 'if not' (...) to require Job Title
+        if len(req_body.get('jobTitle')) < 5:
+            return func.HttpResponse(
+                json.dumps({"version": API_VERSION, "status": 400, "action": "ShowBlockPage",
+                            "code": "SignUp-Validation-04", "userMessage": "Job Title must contain at least five characters."}),
+                status_code=400,
+                mimetype="application/json"
+            )
 
-    # # If jobTitle claim is too short, show validation error message. So, user can fix the input data.
-    if len(req_body.get('jobTitle')) < 4:
-        return func.HttpResponse(
-            json.dumps({"version": "1.0.0", "status": 200, "action": "ShowBlockPage", "code": "SingUp-Validation-03", "userMessage": "Display name must contain at least four characters."}),
-            status_code=200,
-            mimetype="application/json"
-         )
-    
     # Input validation passed successfully, return `Allow` response.
-    return func.HttpResponse("Ok")
-
+    return func.HttpResponse(
+        json.dumps({"version": API_VERSION, "action": "Continue"}),
+        status_code=200,
+        mimetype="application/json"
+    )
